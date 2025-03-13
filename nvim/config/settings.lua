13c32bd44a5299be688698f46185bd73bc5562c3
Lucky K.B. Kim <32375937+luckuisha@users.noreply.github.com>
2025-03-13 14:07:18 -0400

    Create settings.lua

---
+++ nvim/config/settings.lua
@@ -0,0 +1,35 @@
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable syntax highlighting
+vim.cmd('syntax on')
+
+-- Enable mouse support
+vim.opt.mouse = 'a'
+
+-- Set tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+-- Undo file
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Enable relative line numbers
+vim.o.relativenumber = true
+
